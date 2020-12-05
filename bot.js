ac957cc6fd6fb161893a5e6bc692d013f538a165
arhammusheer <arhammusheer@gmail.com>
2020-12-06 02:39:09 +0530

    added logging

--- bot.js
+++ bot.js
@@ -15,23 +15,35 @@ bot.once("ready", () => {
 //On discord message
 bot.on("message", (message) => {
 	//Dad's classic response
-	if (message.content.toLowerCase().startsWith("i'm "))
+	if (message.content.toLowerCase().startsWith("i'm ")) {
+		console.log(
+			`Classic dad response in ${message.guild.name} with guild ID ${message.guild.id}`,
+		);
 		return message.channel.send(`Hi ${message.content.substring(4)}, I'm Dad`);
+	}
 
 	//Dad bot invite
 	if (!message.content.toLowerCase().startsWith(prefix)) return;
 
-	if (message.content.toLowerCase() == `${prefix} invite`)
+	if (message.content.toLowerCase() == `${prefix} invite`) {
+		console.log(
+			`Dad invite sent in ${message.guild.name}  with guild ID ${message.guild.id}`,
+		);
 		return message.channel.send(
 			new Discord.MessageEmbed()
 				.setTitle(`Invite ${name} to your server`)
 				.setURL(process.env.DISCORD_INVITE_LINK),
 		);
+	}
 
 	//Dad jokes
 	if (message.content.toLowerCase() == `${prefix} joke`) {
 		jokeId = Math.floor(Math.random() * jokeLength) + 1;
-        console.log(dadContent.joke[jokeId])
+		dadJoke = dadContent.joke[jokeId];
+		console.log(
+			`dad joke in ${message.guild.name} with guild ID ${message.guild.id}`,
+		);
+		console.log(`Dad joke was: ${dadJoke}`);
         return message.channel.send(dadContent.joke[jokeId]);
 	}
 });
