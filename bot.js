262bbfd9e10a4810cc6cd6d8394b67fcd4d3117d
arhammusheer <arhammusheer@gmail.com>
2020-12-06 02:16:05 +0530

    added dad jokes

--- bot.js
+++ bot.js
@@ -1,18 +1,39 @@
 require("dotenv").config();
 const Discord = require("discord.js");
-const config = require("./config.json");
+const { name, prefix } = require("./config.json");
+const dadContent = require("./content.json");
+
+const jokeLength = Object.keys(dadContent.joke).length;
 
 const bot = new Discord.Client();
 
 //Once bot logs in
 bot.once("ready", () => {
-	console.log(`${config.name} is logged into discord servers`);
+	console.log(`${name} is logged into discord servers`);
 });
 
 //On discord message
 bot.on("message", (message) => {
+	//Dad's classic response
 	if (message.content.toLowerCase().startsWith("i'm "))
 		return message.channel.send(`Hi ${message.content.substring(4)}, I'm Dad`);
+
+	//Dad bot invite
+	if (!message.content.toLowerCase().startsWith(prefix)) return;
+
+	if (message.content.toLowerCase() == `${prefix} invite`)
+		return message.channel.send(
+			new Discord.MessageEmbed()
+				.setTitle(`Invite ${name} to your server`)
+				.setURL(process.env.DISCORD_INVITE_LINK),
+		);
+
+	//Dad jokes
+	if (message.content.toLowerCase() == `${prefix} joke`) {
+		jokeId = Math.floor(Math.random() * jokeLength) + 1;
+        console.log(dadContent.joke[jokeId])
+        return message.channel.send(dadContent.joke[jokeId]);
+	}
 });
 
 //Log into discord
