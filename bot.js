52b94d58b3c718b20b38e805abd6dce2e4a50bbf
arhammusheer <arhammusheer@gmail.com>
2020-12-06 01:30:07 +0530

    added first command

--- bot.js
+++ bot.js
@@ -1,11 +1,18 @@
-require("dotenv").config;
+require("dotenv").config();
 const Discord = require("discord.js");
 const config = require("./config.json");
 
-const bot = new Discord.client();
+const bot = new Discord.Client();
 
+//Once bot logs in
 bot.once("ready", () => {
-	console.log();
+	console.log(`${config.name} is logged into discord servers`);
+});
+
+//On discord message
+bot.on("message", (message) => {
+	if (message.content.toLowerCase().startsWith("i'm "))
+		return message.channel.send(`Hi ${message.content.substring(4)}, I'm Dad`);
 });
 
 //Log into discord
